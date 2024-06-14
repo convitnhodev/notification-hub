f7904d77ae20d465b45d7a8a5384cc728ea7d987
Hung <hungtnv@masvn.local>
2024-06-14 14:50:29 +0700

    chore: update

--- src/adapters/spi/db/db_notification_repository.rs
+++ src/adapters/spi/db/db_notification_repository.rs
@@ -20,4 +20,40 @@ impl NotificationRepositoryAbstract for NotificationRepository {
         let conn = self.db_connection.get_pool().get().expect("couldn't get db connection from pool");
         diesel::insert_into
     }
+
+
+    async fn update_viewed_time_and_status(&self, id: String, user_id: String, viewed_time :SystemTime) -> Result<String, Box<Error>> { 
+
+    }
+
+
+    async fn update_viewed_time_and_status(&self, id: String, user_id: String, viewed_time :SystemTime) -> Result<String, Box<Error>> { 
+        
+    }
+
+
+    async fn update_viewed_time_and_status_for_entire_notification(&self, user_id: String, viewed_time :SystemTime) -> Result<String, Box<Error>> { 
+        
+    }
+
+
+    async fn update_status_by_id(&self, user_id: String, status Status) -> Result<String, Box<Error>> { 
+        
+    }
+
+
+    async fn get_by_id(&self, id String) Result<NotificationEntity, Box<Error>> {
+        let conn = self.db_connection.get_pool().get().expect("couldn't get db connection from pool");
+        let results = dog_facts.load::<Notification>(&conn);
+
+        match results {
+            Ok(models) => Ok(models.into_iter().map(DogFactDbMapper::to_entity).collect::<Vec<DogFactEntity>>()),
+            Err(e) => Err(Box::new(e)),
+        }
+    }
+
+
+
+
+
 }
